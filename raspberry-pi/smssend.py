3bff98459ae6fdca142d42b80e77dfcbe2c90ca1
sanket-sinha <59686512+sanket-sinha@users.noreply.github.com>
2021-06-29 15:16:51 +0530

    Update smssend.py

--- raspberry-pi/smssend.py
+++ raspberry-pi/smssend.py
@@ -1,18 +1,16 @@
-#api: /ZrCUoI7WZE-jNAyhzMFEcA5HLDjQqw58RGGPtCurC
+
 
 #!/usr/bin/env python
  
 import urllib.request
 import urllib.parse
-
+apikey= "<apikey>"
 
 def sendSMS(numbers, message):
-    params = urllib.parse.urlencode({'apikey': '/ZrCUoI7WZE-jNAyhzMFEcA5HLDjQqw58RGGPtCurC', 'numbers': numbers, 'message': message})
+    params = urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers, 'message': message})
     params = params.encode('utf-8')
     request = urllib.request.Request("https://api.textlocal.in/send/?")
     f = urllib.request.urlopen(request, params)
     return (f.read())
 
 
-#resp = sendSMS('918697859907', 'My location is: http://maps.google.com/?q=22.5445,84.4612')
-#print(resp)
